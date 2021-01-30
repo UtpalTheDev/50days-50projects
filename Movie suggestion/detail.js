b6800d34c69610ed78add28e006e44ea2b68a997
UtpalTheDev <utpalpati8@gmail.com>
2021-01-30 22:23:39 +0530

    refactor: imdb id returning from api

--- Movie suggestion/detail.js
+++ Movie suggestion/detail.js
@@ -1,5 +1,6 @@
 const img_path='https://image.tmdb.org/t/p/w1280';
-const search_url='https://api.themoviedb.org/3/search/movie?api_key= =';
+const api='';
+
 
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;
@@ -12,7 +13,47 @@ function getParameterByName(name, url) {
 }
 
 var id = getParameterByName('#########rtddrrdr#%%%%%%%');
+const search_url=`https://api.themoviedb.org/3/movie/${id}?api_key=${api}`;
+const external_url=`https://api.themoviedb.org/3/movie/${id}/external_ids?api_key=${api}`;
 
-function
+console.log(search_url);
+call(search_url);
+
+function call(search_url){
+    fetch(search_url)
+    .then(response=>response.json())
+    .then(json=>{
+        console.log(json);
+    })
+    .catch(errorhandle)
+       
+}
+externalid(external_url);
+
+function externalid(external_url){
+    fetch(external_url)
+    .then(response=>response.json())
+    .then(json=>{
+        console.log(json);
+        person(json.imdb_id);
+    })
+    .catch(errorhandle)
+       
+}
+
+function person(imdbid){
+    person_url=`https://api.themoviedb.org/3/find/${imdbid}?api_key=${api}&language=en-US&external_source=imdb_id`;
+    fetch(person_url)
+    .then(response=>response.json())
+    .then(json=>{
+        console.log(json);
+        
+    })
+    .catch(errorhandle)
+        
+}
+function errorhandle(){
+    console.log('error');
+}
 
 
